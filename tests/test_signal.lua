141252fb903e86642c0940f4c93d47c1ae915361
katcipis <tiagokatcipis@gmail.com>
2010-01-08 12:22:16 -0200

    fixing comments to be more lua-ish

--- tests/test_signal.lua
+++ tests/test_signal.lua
@@ -1,25 +1,24 @@
---[[
-   Copyright (C) 2010 Digitro Corporation <www.digitro.com.br>
+---------------------------------------------------------------------------------
+-- Copyright (C) 2010 Digitro Corporation <www.digitro.com.br>
+-- 
+-- @author Paulo Pizarro  <paulo.pizarro@gmail.com>
+-- @author Tiago Katcipis <tiagokatcipis@gmail.com>
  
-   @author Paulo Pizarro  <paulo.pizarro@gmail.com>
-   @author Tiago Katcipis <tiagokatcipis@gmail.com>
+-- This file is part of LuaNotify.
  
-   This file is part of LuaNotify.
+-- LuaNotify is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
  
-   LuaNotify is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+-- LuaNotify is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
  
-   LuaNotify is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
- 
-   You should have received a copy of the GNU Lesser General Public License
-   along with Luasofia.  If not, see <http://www.gnu.org/licenses/>.
---]]
-
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with Luasofia.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------
 
 require "lunit"
 require "luanotify.signal"
