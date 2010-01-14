d9d294228efde744dfefb37f57127147a558432b
katcipis <tiagokatcipis@gmail.com>
2010-01-14 18:36:34 -0200

    -closes #6 and extends the idea to connect too

--- luanotify/signal.lua
+++ luanotify/signal.lua
@@ -24,24 +24,6 @@
 
 Signal = {} --Class attributes and methods goes on this table.
 
--- private functions
-
-local function get_handler_table(handlers, handler_function)
-    for pos, handler_table in ipairs(handlers) do
-        if(handler_table.handler == handler_function) then
-            return pos, handler_table
-        end
-    end
-end
-
-local function get_function_position(func_table, target_func)
-    for pos, func in ipairs(func_table) do
-        if(func == target_func) then
-            return pos
-        end
-    end
-end
-
 
 -- Class definition and methods
 
@@ -63,8 +45,11 @@ end
 
 
 function Signal:disconnect(handler_function)
-    local pos, _ = get_handler_table(self.handlers, handler_function)
-    if(pos) then table.remove(self.handlers, pos) end
+    local pos = self.handlers[handler_function]
+    if(pos) then 
+        table.remove(self.handlers, pos)
+        self.handlers[handler_function] = nil
+    end
 end
 
 
@@ -73,27 +58,33 @@ function Signal:connect(handler_function)
         error("connect: expected a function, got a "..type(handler_function));
     end
 
-    if(not get_handler_table(self.handlers, handler_function)) then
-        table.insert(self.handlers, { handler = handler_function,
-                                      block   = 0 })
+    if(not self.handlers[handler_function]) then
+        local pos = #self.handlers + 1
+        self.handlers[handler_function] = pos
+        self.handlers[pos] = { handler = handler_function, 
+                               block = 0 }
     end
 end
 
 
 function Signal:block(handler_function)
-    local _, handler_table = get_handler_table(self.handlers, handler_function)
-    if(handler_table) then
-        handler_table.block = handler_table.block + 1
+    if(self.handlers[handler_function]) then
+        local handler_table = self.handlers[self.handlers[handler_function]]
+        if(handler_table) then
+            handler_table.block = handler_table.block + 1
+        end
     end
 end
 
 
 function Signal:unblock(handler_function)
-    local _, handler_table = get_handler_table(self.handlers, handler_function)
-    if(handler_table) then
-        if(handler_table.block > 0) then
-            handler_table.block = handler_table.block - 1
-        end
+   if(self.handlers[handler_function]) then
+       local handler_table = self.handlers[self.handlers[handler_function]]
+       if(handler_table) then
+           if(handler_table.block > 0) then
+               handler_table.block = handler_table.block - 1
+           end
+       end
     end
 end
 
@@ -139,32 +130,42 @@ function Signal:add_pre_emit(pre_emit_func)
         error("add_pre_emit: expected a function, got a "..type(pre_emit_func));
     end
 
-    if(not get_function_position(self.pre_emit_funcs, pre_emit_func)) then
-        table.insert(self.pre_emit_funcs, pre_emit_func)
+    if(not self.pre_emit_funcs[pre_emit_func]) then
+        local pos = #self.pre_emit_funcs + 1
+        self.pre_emit_funcs[pre_emit_func] = pos
+        self.pre_emit_funcs[pos] = pre_emit_func
     end
 end
 
 
 function Signal:remove_pre_emit(pre_emit_func)
-    local pos = get_function_position(self.pre_emit_funcs, pre_emit_func)
-    if(pos) then table.remove(self.pre_emit_funcs, pos) end
+    local pos = self.pre_emit_funcs[pre_emit_func]
+    if(pos) then
+        table.remove(self.pre_emit_funcs, pos)
+        self.pre_emit_funcs[pre_emit_func] = nil
+    end
 end
 
 
 function Signal:add_post_emit(post_emit_func)
     if (type(post_emit_func) ~= "function") then
-        error("add_post_emit: expected a function, got a "..type(post_emit_func));
+        error("add_post_emit: expected a function, got a "..type(pre_emit_func));
     end
 
-    if(not get_function_position(self.post_emit_funcs, post_emit_func)) then
-        table.insert(self.post_emit_funcs, post_emit_func)
+    if(not self.post_emit_funcs[post_emit_func]) then
+        local pos = #self.post_emit_funcs + 1
+        self.post_emit_funcs[post_emit_func] = pos
+        self.post_emit_funcs[pos] = post_emit_func
     end
 end
 
 
 function Signal:remove_post_emit(post_emit_func)
-    local pos = get_function_position(self.post_emit_funcs, post_emit_func)
-    if(pos) then table.remove(self.post_emit_funcs, pos) end
+    local pos = self.post_emit_funcs[post_emit_func]
+    if(pos) then
+        table.remove(self.post_emit_funcs, pos)
+        self.post_emit_funcs[post_emit_func] = nil
+    end
 end
 
 
