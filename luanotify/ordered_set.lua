a24265aa20544e86b5303dd494bd2dc5d278f46e
katcipis <tiagokatcipis@gmail.com>
2010-02-04 22:08:11 -0200

    adding OrderedSet interface -closes #9 -closes #10 -closes #11

---
+++ luanotify/ordered_set.lua
@@ -0,0 +1,65 @@
+---------------------------------------------------------------------------------
+-- Copyright (C) 2010 Tiago Katcipis <tiagokatcipis@gmail.com>
+-- Copyright (C) 2010 Paulo Pizarro  <paulo.pizarro@gmail.com>
+-- 
+-- @author Paulo Pizarro  <paulo.pizarro@gmail.com>
+-- @author Tiago Katcipis <tiagokatcipis@gmail.com>
+
+-- This file is part of LuaNotify.
+
+-- LuaNotify is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- LuaNotify is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with Luasofia.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------
+module(..., package.seeall)
+
+-----------------------------------------------------
+-- Class attributes and methods goes on this table --
+-----------------------------------------------------
+local OrderedSet = {}
+
+------------------------------------
+-- Metamethods goes on this table --
+------------------------------------
+local OrderedSet_mt = { __index = OrderedSet, __metatable = "protected" }
+
+
+--------------------------
+-- Constructor function --
+--------------------------
+
+function new ()
+    local object = {}
+    -- set the metatable of the new object as the OrderedSet_mt table (inherits OrderedSet).
+    setmetatable(object, OrderedSet_mt)
+
+    -- create all the instance state data.
+    return object
+end
+
+
+----------------------------------
+-- Class definition and methods --
+----------------------------------
+
+function OrderedSet:push_front(data)
+end
+
+function OrderedSet:push_back(data)
+end
+
+function OrderedSet:get_iterator()
+end
+
+function OrderedSet:remove(data)
+end
+
