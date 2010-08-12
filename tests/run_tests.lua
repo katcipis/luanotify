64a926be2915d4e1e38c85f9e43cb7c0b4a9237a
Tiago Katcipis <tiagokatcipis@gmail.com>
2010-08-12 16:37:32 -0300

    adding new script to run all tests (makes validation of a refactoring easier)

---
+++ tests/run_tests.lua
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+
+-- Copyright (C) 2010 Tiago Katcipis <tiagokatcipis@gmail.com>
+-- Copyright (C) 2010 Paulo Pizarro  <paulo.pizarro@gmail.com>
+
+-- @author Paulo Pizarro  <paulo.pizarro@gmail.com>
+-- @author Tiago Katcipis <tiagokatcipis@gmail.com>
+
+-- This file is part of LuaNotify.
+
+-- LuaNotify is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- LuaNotify is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with LuaNotify.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------
+
+print("<<<<<<<<<<<<< STARTING TESTS >>>>>>>>>>>>>")
+
+dofile("test_double_queue.lua")
+dofile("test_global_event.lua")
+dofile("test_instance_event.lua")
+dofile("test_signal.lua")
+
+print("<<<<<<<<<<<<< TESTS ENDED>>>>>>>>>>>>>")
